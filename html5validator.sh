5592b153b9a6096bdf8d3dd0c7bcdbd2cf01e592
Artur Pedziwilk <wilkart@mac.com>
2025-04-16 02:00:14 +0200

    add  --also-check-css

--- html5validator.sh
+++ html5validator.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -v $(pwd)/site:/mnt cyb3rjak3/html5validator:latest html5validator --root /mnt
+docker run -v $(pwd)/site:/mnt cyb3rjak3/html5validator:latest html5validator --log INFO --format text --root /mnt --also-check-css
